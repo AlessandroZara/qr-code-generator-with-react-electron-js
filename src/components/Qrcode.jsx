127870aa27bad0d53e23845fdb9a565d0107e9be
Alessandro Zara <alezara@hotmail.it>
2023-04-07 15:43:50 +0200

    added possibility to click with enter key

--- src/components/Qrcode.jsx
+++ src/components/Qrcode.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/alt-text */
 import QRCode from "qrcode";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Button } from "@mui/material";
 
 function Qrcode() {
@@ -8,52 +8,60 @@ function Qrcode() {
   const [qr, setQr] = useState("");
   const [error, setError] = useState("");
 
-  const pattern = new RegExp('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?'); 
-   const check = pattern.test(url)
+  const pattern = new RegExp(
+    "(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?"
+  );
+  const check = pattern.test(url);
 
-  const GenerateQRCode = () => {	  
-	if(url === "https://"){
-		setError("Inserisci un indirizzo url perfavore")
-		setQr("");
-		setUrl("https://");
-	}else if(check === false ){
-		setError("Inserisci un indirizzo url valido perfavore")
-		setQr("");
-		setUrl("https://");
-	}
-	else{
-		QRCode.toDataURL(
-			url,
-			{
-			  width: 800,
-			  margin: 2,
-			  color: {
-				// black: '#335383FF',
-				// blue: '#EEEEEEFF'
-			  },
-			},
-			(err, url) => {
-			  if (err) return console.error(err);
-	  
-			  setQr(url);
-			}
-		  );
-	}
-    
+  const GenerateQRCode = () => {
+    if (url === "https://") {
+      setError("Inserisci un indirizzo url perfavore");
+      setQr("");
+      setUrl("https://");
+    } else if (check === false) {
+      setError("Inserisci un indirizzo url valido perfavore");
+      setQr("");
+      setUrl("https://");
+    } else {
+      QRCode.toDataURL(
+        url,
+        {
+          width: 800,
+          margin: 2,
+          color: {
+            // black: '#335383FF',
+            // blue: '#EEEEEEFF'
+          },
+        },
+        (err, url) => {
+          if (err) return console.error(err);
+
+          setQr(url);
+        }
+      );
+    }
   };
   const ResetQRCode = () => {
     setQr("");
     setUrl("https://");
   };
+  
+  useEffect(() => {
+    window.addEventListener('keydown', e => {
+     if(e.key === 'Enter'){
+      GenerateQRCode();
+     }
+    })
+   })
 
   useEffect(() => {
-	if (error){
-	  setTimeout(function() {
-		setError("")
-		
-	  }, 2000);
-	}});
-  
+    if (error) {
+      setTimeout(function () {
+        setError("");
+      }, 2000);
+    }
+  });
+
   return (
     <div className="app">
       <h1>QR Generator</h1>
@@ -66,6 +74,7 @@ function Qrcode() {
       <Button variant="contained" onClick={GenerateQRCode}>
         Generate
       </Button>
+    
       {qr && (
         <>
           <Button
@@ -89,11 +98,11 @@ function Qrcode() {
             Download
           </Button>
         </>
-      ):
-	  <>
-		<p style={{fontSize:"20px"}}>{error}</p>
-	  </>
-	  }
+      ) : (
+        <>
+          <p style={{ fontSize: "20px" }}>{error}</p>
+        </>
+      )}
     </div>
   );
 }
